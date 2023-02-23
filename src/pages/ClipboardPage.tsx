b6f3133e2ad29871531f035f302658ce35a05ac6
Deveesh Shetty <deveeshshetty@gmail.com>
2023-02-23 19:01:35 +0530

    Add skeletal design for copy entire palette

--- src/pages/ClipboardPage.tsx
+++ src/pages/ClipboardPage.tsx
@@ -41,27 +41,57 @@ function ClipboardPage() {
           setNotification={setNotification}
         />
       )}
-      <h2 className="text-center font-bold text-3xl text-primary-100">
+      <h2 className="text-center font-bold text-4xl text-primary-100">
         Clipboard History
       </h2>
-      <div className="m-2 p-2">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
-          {clipboard?.map((color, index) => (
-            <motion.div
-              key={index}
-              initial={{ y: 100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: -100, opacity: 0 }}
-              className="flex flex-col items-center gap-2"
-            >
-              <div
-                className="h-36 aspect-square cursor-pointer rounded-lg"
-                onClick={() => onClick(color)}
-                style={{ backgroundColor: color }}
-              ></div>
-              <p>{color}</p>
-            </motion.div>
-          ))}
+      <div className="grid lg:grid-cols-[40%_60%] m-2 p-2 my-8 gap-4">
+        <div className="flex flex-col gap-4">
+          <p className="text-center font-bold text-3xl text-primary-100">
+            Pallete
+          </p>
+          <div className="flex flex-col items-center gap-2 mx-auto">
+            <div className="flex">
+              {clipboard?.map((color, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ y: 100, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -100, opacity: 0 }}
+                  className="flex flex-col items-center"
+                >
+                  <div
+                    className="h-24 aspect-square cursor-pointer"
+                    onClick={() => onClick(color)}
+                    style={{ backgroundColor: color }}
+                  ></div>
+                </motion.div>
+              ))}
+            </div>
+            <p>Pallete Name</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-center font-bold text-3xl text-primary-100">
+            Colors
+          </p>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+            {clipboard?.map((color, index) => (
+              <motion.div
+                key={index}
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -100, opacity: 0 }}
+                className="flex flex-col items-center gap-2"
+              >
+                <div
+                  className="h-36 aspect-square cursor-pointer rounded-lg"
+                  onClick={() => onClick(color)}
+                  style={{ backgroundColor: color }}
+                ></div>
+                <p>{color}</p>
+              </motion.div>
+            ))}
+          </div>
         </div>
       </div>
       <div className="fixed bottom-6 right-6 md:bottom-8 md:right-8">
